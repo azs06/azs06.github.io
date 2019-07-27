8adf6b85a3e23e2ac94b3d14e14e1456a2b8fe8a
Md.Atiquzzaman Soikat <atik.aiub@gmail.com>
2019-07-28 01:41:58 +0600

    added blog

---
+++ eleventy/.eleventy.js
@@ -0,0 +1,20 @@
+const moment = require('moment');
+ 
+moment.locale('en');
+
+module.exports = function(eleventyConfig){
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+ 
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+	return {
+	    dir: {
+	        input: "./",
+	        output: "../"
+	    }
+	}
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+### Md. Atiquzzaman Soikat's Website
+
+### Todos
+
+- [ ] Add proper css to blog page
+- [ ] Convert whole website to eleventy
+- [ ] Add pagination support
+- [ ] Add 404 page
+- [ ] Use PostCSS
+- [ ] Add proper blog features
