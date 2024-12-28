af39b599bd5c5a118ee4f42b8c73ccec93a5dc28
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2024-12-29 03:21:59 +0600

    added projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [mdx(), sitemap(), react(), tailwind()],
 });
