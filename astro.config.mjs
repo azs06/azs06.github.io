73e36637a6450b4b344f2315c9ca11a1a1338ef9
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-20 19:08:26 +0600

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://soikat.dev',
     integrations: [mdx(), sitemap(), react(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Md Atiquzzaman Soikat';
+export const SITE_DESCRIPTION = 'Personal Website of Md Atiquzzaman Soikat';
