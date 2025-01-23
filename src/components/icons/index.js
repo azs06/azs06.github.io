effc0742186bc153c60b071e5a2ba21fdcebf6e0
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-24 03:50:14 +0600

    updated expertise

---
+++ src/components/icons/index.js
@@ -0,0 +1,25 @@
+import Astro from "./Astro.astro";
+import Css3 from "./Css3.astro";
+import Html5 from "./Html5.astro";
+import Js from "./Js.astro";
+import Next from "./Next.astro";
+import React from "./React.astro";
+import Svelte from "./Svelte.astro";
+import Typescript from "./Typescript.astro";
+import Vue from "./Vue.astro";
+import Eleventy from "./Eleventy.astro";
+import Xstate from "./Xstate.astro";
+
+export {
+    Astro,
+    Css3,
+    Html5,
+    Js,
+    Next,
+    React,
+    Svelte,
+    Typescript,
+    Vue,
+    Eleventy,
+    Xstate
+}
