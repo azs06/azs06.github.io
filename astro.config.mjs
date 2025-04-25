b13b3820c065ae3b2f2e60b265fe6ffd19568f50
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-04-25 23:18:47 +0600

    removed react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://soikat.dev',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind()],
 });
